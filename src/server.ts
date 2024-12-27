3747808f23fba383a64448a206518f162f3892a3
Luiz Ricardo <luizricardo41@gmail.com>
2024-12-27 14:43:29 -0300

    Created and separated file app from server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,4 @@
-import fastify from 'fastify'
-import { knex } from './database'
+import { app } from './app'
+import { env } from './env'
 
-const app = fastify()
-
-app.get('/', async () => {
-  return await knex('sqlite_schema').select()
-})
-
-app.listen({ port: 3001 }).then(() => console.log('HTTP Server Running!'))
+app.listen({ port: env.PORT }).then(() => console.log('HTTP Server Running!'))
