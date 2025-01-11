30193614794ce67368f95fde04f14bef11ad2336
Luiz Ricardo <luizricardo41@gmail.com>
2025-01-11 17:39:46 -0300

    Route to update the meal created

---
+++ src/@types/ObjectUpdate.d.ts
@@ -0,0 +1,6 @@
+export type ObjectUpdate = {
+  name?: string
+  description?: string
+  datetime?: Date
+  is_diet?: boolean
+}
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -3,6 +3,7 @@ import { knex } from '../database'
 import { isUserLoggedIn } from '../middlewares/check-user-is-logged-in'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
+import { ObjectUpdate } from '../@types/ObjectUpdate'
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.post(
@@ -71,7 +72,87 @@ export async function mealsRoutes(app: FastifyInstance) {
         .where('user_id', user.id)
         .select()
 
-      return { meals }
+      const formattedInfosMeals = meals.map((meal) => {
+        return {
+          ...meal,
+          datetime: new Date(meal.datetime).toISOString(),
+        }
+      })
+
+      return { meals: formattedInfosMeals }
+    },
+  )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [isUserLoggedIn],
+    },
+    async (req, reply) => {
+      const { sessionId } = req.cookies
+
+      const user = await knex('users')
+        .where('session_id', sessionId)
+        .select('id')
+        .first()
+
+      if (!user) {
+        return reply.status(404).send({ message: 'User not found' })
+      }
+
+      const idSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { success, data, error } = idSchema.safeParse(req.params)
+
+      if (!success) {
+        return reply.status(400).send({ message: error.errors[0].message })
+      }
+
+      const { id } = data
+
+      const meal = await knex('meals_register')
+        .where({ id, user_id: user.id })
+        .select()
+        .first()
+
+      if (!meal) {
+        return reply.status(404).send({ message: 'Meal not found' })
+      }
+
+      if (JSON.stringify(req.body) === '{}') {
+        return reply.status(304).send()
+      }
+
+      const bodySchema = z.object({
+        name: z.string().min(3).optional(),
+        description: z.string().min(5).optional(),
+        datetime: z.string().optional(),
+        isDiet: z.boolean().optional(),
+      })
+
+      const {
+        success: successBody,
+        data: dataBody,
+        error: errorBody,
+      } = bodySchema.safeParse(req.body)
+
+      if (!successBody) {
+        return reply.status(400).send({ message: errorBody.errors[0].message })
+      }
+
+      const { name, description, datetime, isDiet } = dataBody
+      const objUpdate = {} as ObjectUpdate
+
+      if (name) objUpdate.name = name
+      if (description) objUpdate.description = description
+      if (datetime) objUpdate.datetime = new Date(datetime)
+      if (isDiet) objUpdate.is_diet = isDiet
+
+      await knex('meals_register').where('id', id).update(objUpdate)
+
+      return reply.status(201).send({ message: 'Meal Updated' })
     },
   )
 }
